291bfbc25fcc1360a47b87d4e3e833653c0daad7
flier268 <flier268@gmail.com>
2020-05-14 09:43:22 +0800

    remove System.Management and add a function

--- Bluetooth/PublicFunction.cs
+++ Bluetooth/PublicFunction.cs
@@ -1,49 +1,28 @@
-﻿using System.Management;
+﻿using System;
+using System.Diagnostics;
 
 namespace Bluetooth.GATT
 {
-    public class PublicFunction
+    public static class PublicFunction
     {
+        /// <summary>
+        /// Is the OS support BLE or not?
+        /// OS >= Win 10.17134
+        /// </summary>
         public static bool WinRT_API_Support
         {
             get
             {
-                string[] version = GetOsVer().Split('.');
-                if (int.Parse(version[0]) >= 10 && int.Parse(version[2]) >= 17134)
-                    return true;
-                else
-                    return false;
+                var version = Environment.OSVersion;
+                return version.Version.Major >= 10 && version.Version.Build >= 17134;
             }
         }
-
-        private static ManagementObject GetMngObj(string className)
+        /// <summary>
+        /// Open the BLE setting page from system settings
+        /// </summary>
+        public static void OpenBLESetting()
         {
-            var wmi = new ManagementClass(className);
-
-            foreach (var o in wmi.GetInstances())
-            {
-                var mo = (ManagementObject)o;
-                if (mo != null) return mo;
-            }
-
-            return null;
-        }
-
-        public static string GetOsVer()
-        {
-            try
-            {
-                ManagementObject mo = GetMngObj("Win32_OperatingSystem");
-
-                if (null == mo)
-                    return string.Empty;
-
-                return mo["Version"] as string;
-            }
-            catch
-            {
-                return string.Empty;
-            }
+            Process.Start("ms-settings-bluetooth:");
         }
     }
 }
